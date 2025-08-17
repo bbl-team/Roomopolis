816ffb3a4d13841d628df1fdac4bc4c9cdd00edf
benbenlaw <benlawrenceben@hotmail.co.uk>
2025-08-17 09:34:43 +0100

    2.6.1

--- src/main/java/com/benbenlaw/roomopolis/network/RoomopolisMessages.java
+++ src/main/java/com/benbenlaw/roomopolis/network/RoomopolisMessages.java
@@ -1,6 +1,6 @@
 package com.benbenlaw.roomopolis.network;
 
-import com.benbenlaw.caveopolis.Caveopolis;
+import com.benbenlaw.Roomopolis;
 import com.benbenlaw.roomopolis.network.packet.GetStructurePalettePacket;
 import com.benbenlaw.roomopolis.network.packet.GetStructureSizePacket;
 import com.benbenlaw.roomopolis.network.payload.GetStructurePalettePayload;
@@ -11,7 +11,7 @@ import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 public class RoomopolisMessages {
 
     public static void registerNetworking(final RegisterPayloadHandlersEvent event) {
-        final PayloadRegistrar registrar = event.registrar(Caveopolis.MOD_ID);
+        final PayloadRegistrar registrar = event.registrar(Roomopolis.MOD_ID);
 
         //To Client From Server
         registrar.playToClient(GetStructureSizePayload.TYPE, GetStructureSizePayload.STREAM_CODEC, GetStructureSizePacket.get()::handle);
